8611849ce8952ea6878d03fb5d8cdc0f9cd38f67
Paulo Schiavon <paulo@lptennis.com>
2014-05-22 20:33:41 -0300

    refactoring validations of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
-  validates_length_of :bio, minimum: 30, allow_blank: false
-  validates_uniqueness_of :email
+  validates :full_name, :location, presence: true
   
-  # Essa validação pode ser representada da seguinte forma:
-  # validates_format_of :email, with: EMAIL_REGEXP
-  validate do
-    errors.add(:email,:invalid) unless email.match(EMAIL_REGEXP)
-  end
-    
+  validates :password,  presence: true, 
+                        confirmation: true
+  
+  validates :email,     presence: true, 
+                        format: { with: /\A[^@]+@[^@\.]+\z/ }, 
+                        uniqueness: true                   
+                        
+  validates_length_of :bio, minimum: 30, 
+                            allow_blank: false
 end
