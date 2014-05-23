d07f63fda274810ee0ce407f6c1a06cf63f33160
Paulo Schiavon <paulo@lptennis.com>
2014-05-23 15:49:36 -0300

    add validation of the rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ActiveRecord::Base
+  
+  validates :title, :location, :description, presence: true
+  
+  validates_length_of :description, minimum: 30, 
+                      allow_blank: false
+  
   def complete_name
     "#{title}, #{location}"
   end
