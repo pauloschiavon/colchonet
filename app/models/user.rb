aa27809a79a4ea3a72f8dcf0d74e91e422607bfd
Paulo Schiavon <paulo@lptennis.com>
2014-05-22 21:32:08 -0300

    security fix. add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ActiveRecord::Base
   
   validates :full_name, :location, presence: true
   
-  validates :password,  presence: true, 
-                        confirmation: true
-  
   validates :email,     presence: true, 
                         format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, 
                         uniqueness: true                   
                         
   validates_length_of :bio, minimum: 30, 
                             allow_blank: false
+  
+  has_secure_password
 end
